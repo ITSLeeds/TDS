a7ea9aa3a9cb607243eee5c350e99f43b8e378f9
Robin Lovelace <rob00x@gmail.com>
2019-02-06 22:59:45 +0000

    Add slides

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ cp -Rv slides /tmp
 git checkout gh-pages
 cp -Rv /tmp/slides/* slides/
 git status
-git add slides/1-intro* slides/libs
+git add slides/* slides/libs
 git commit -am 'Add slides'
 git push origin gh-pages
 git clean -f

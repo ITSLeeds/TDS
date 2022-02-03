5967d2f04e1ccb91084e0186ddeabbafb891a794
Robin Lovelace <rob00x@gmail.com>
2022-02-03 11:42:34 +0000

    Update practical 1 notes

--- messages/practical1-notes.R
+++ messages/practical1-notes.R
@@ -10,3 +10,46 @@ pkgs = c(
 )
 remotes::install_cran(pkgs)
 remotes::install_github("nowosad/spDataLarge")
+
+install.packages("terra", type = "binary")
+?install_cran
+od_data = od_data_sample
+
+names(od_data)
+# Ctl + Shift + M
+od_data_walk = od_data %>% 
+  rename(walk = foot) %>% 
+  filter(walk > 0) %>% 
+  select(geo_code1, geo_code2, all, car_driver, walk, bicycle) %>% 
+  mutate(
+    proportion_walk = walk / all,
+    proportion_drive = car_driver / all
+  )
+class(od_data)
+
+# calculate the % of OD have at least 1 person walking?
+nrow(od_data_walk) / nrow(od_data) * 100
+od_data_walk_cycle = od_data_walk %>% 
+  mutate(pcycle = bicycle / all)
+
+# Exercise 3
+cor(od_data_walk_cycle$proportion_walk, od_data_walk_cycle$pcycle)
+plot(od_data_walk_cycle$proportion_walk, od_data_walk_cycle$pcycle)
+od_data_walk_cycle %>% 
+  ggplot() +
+  geom_point(aes(proportion_walk, pcycle)) +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_continuous(labels = scales::percent) 
+
+?od_data_sample
+zones_sf
+summary(zones_sf$geo_code %in% od_data$geo_code1)
+
+od_data_sf = od::od_to_sf(x = od_data, z = zones_sf)
+library(tmap)
+tmap_mode("view")
+tm_shape(od_data_sf) +
+  tm_lines()
+
+
+
